e3c6cae1bff89ff3cd9ea289252ef4dcf2b85c19
Calla Rudolph and Max Scher <calla@>
2017-06-06 13:45:35 -0700

    add additional functions to retrieve values from each question input

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,23 @@ $(document).ready(function() {
   $(".test").submit(function(event) {
     event.preventDefault();
 
-    var count = 0;
+    var warningCount = 0;
     $("input:checkbox[name=warning]:checked").each(function() {
       var stressors = parseInt($(this).val());
-      count += stressors;
+      warningCount += stressors;
     });
+    var symptomCount = 0;
+    $("input:checkbox[name=symptom]:checked").each(function() {
+      var stressors = parseInt($(this).val());
+      symptomCount += stressors;
+    });
+    var copeCount = 0;
+    $("input:checkbox[name=cope]:checked").each(function() {
+      var stressors = parseInt($(this).val());
+      copeCount += stressors;
+    });
+
+
     $(".guidance").hide();
     if (count >= 4) {
       $("#rough").show();
