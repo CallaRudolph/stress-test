bd5633f91970dcd9a2906e7faa94aedd8aed54a1
Calla Rudolph and Max Scher <calla@>
2017-06-06 13:35:21 -0700

    change values of warning inputs to 1, add one logic rule to js for warning inputs

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,21 @@ $(document).ready(function() {
   $(".test").submit(function(event) {
     event.preventDefault();
 
+    var count = 0;
+    $("input:checkbox[name=warning]:checked").each(function() {
+      var stressors = parseInt($(this).val());
+      count += stressors;
+    });
+    $(".guidance").hide();
+    if (count >= 4) {
+      $("#rough").show();
+    } else {
+      $("#good").show();
+    }
+
+
+    // $("#rough").show();
+    // $("input:")
 
 
   });
